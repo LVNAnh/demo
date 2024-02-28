fdfc979e0360171da6c8a6c4c3c050b5c1fe8585
LVNAnh <levunhatanh1997@gmail.com>
2024-02-28 14:46:08 +0700

    Add new project demo

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$conn=mysqli_connect('localhost','root','','myshopping_cart');
+if($conn)
+{
+    echo "";
+}
+else{
+    die("Failed to connect");
+}
+?>
---
+++ view_products.php
@@ -0,0 +1,75 @@
+<!--including php logic - connecting to database-->
+<?php include 'connect.php'?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Products-Project</title>
+
+    <!--css file-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--fontawesome link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.css" 
+    integrity="sha512-tx5+1LWHez1QiaXlAyDwzdBTfDjX07GMapQoFTS74wkcPMsI3So0KYmFe6EHZjI8+eSG0ljBlAQc3PQ5BTaZtQ==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!--header-->
+    <?php include 'header.php'?>
+    <!--container-->
+    <div class="container">
+        <section class="display_product">
+            
+                    <!--php code-->
+                    <?php
+                    $display_product = mysqli_query($conn, "Select * from `products` ");
+                    $num=1;
+                    if(mysqli_num_rows($display_product)>0)
+                    {
+                        echo "<table>
+                <thead>
+                    <th>Sl No</th>
+                    <th>Product Image</th>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>";
+                        //logic to fetch data
+                        while($row = mysqli_fetch_assoc($display_product)){
+                            ?>
+                        <!--display table-->
+                        
+                        <tr>
+                        <td><?php echo $num ?></td>
+                        <td><img src="images/<?php echo $row['image']?>" alt=<?php echo $row['name'];?> ></td>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['price']; ?></td>
+                        <td>
+                            <a href="delete.php?delete=<?php echo $row['id']?>" 
+                            class="delete_product_btn" onclick="return confirm('Are you sure you want to delete this product?');">
+                            <i class="fas fa-trash"></i></a>
+                            <a href="update.php?edit=<?php echo $row['id']?>" class="update_product_btn">
+                            <i class="fas fa-edit"></i></a>
+                        </td>
+                    </tr>
+                            <?php
+                            $num++;
+                        }
+                        
+                    }
+                    else
+                    {
+                        echo "<div class='empty_text'>No Products Available</div>";
+                    }
+                    ?>
+                    
+                </tbody>
+            </table>
+        </section>
+</body>
+</html>
